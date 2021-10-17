b5c0540c67330c4b99a22770cd9ccda0c5dcf4e4
Erik Andersen <jensenerik81@gmail.com>
2021-10-17 15:08:46 +0200

    refactor build trajectory

---
+++ src/ch_filter.rs
@@ -0,0 +1,148 @@
+use geo::prelude::HaversineDistance;
+
+pub struct CHFilter<I: Iterator<Item = [f64; 3]>> {
+    stream: I,
+    window: Vec<[f64; 3]>,
+    window_size: usize,
+}
+
+impl<I: Iterator<Item = [f64; 3]>> CHFilter<I> {
+    pub fn new(window_size: usize, stream: I) -> CHFilter<I> {
+        CHFilter {
+            stream,
+            window: Vec::new(),
+            window_size,
+        }
+    }
+
+    pub fn next(&mut self) -> Option<[f64; 3]> {
+        let mut trj: Vec<[f64; 3]> = Vec::new();
+        while trj.len() < self.window_size {
+            print!("1");
+            // Fill the window
+            while self.window.len() < self.window_size {
+                print!("2");
+                let point: Option<[f64; 3]> = self.stream.next();
+                if let Some(point) = point {
+                    self.window.push(point);
+                } else {
+                    // Nothing to read from stream. Reflect this in return
+                    return None;
+                }
+            }
+            // Create the convex hull and filter the spikes
+            trj = get_convex_hull_trj(self.window.clone());
+            let spikes: Vec<[f64; 3]> = get_spikes(trj);
+            trj = remove_spikes(self.window.clone(), spikes);
+        }
+        self.window = trj;
+        Some(self.window.remove(0))
+    }
+}
+
+fn get_convex_hull_trj(points: Vec<[f64; 3]>) -> Vec<[f64; 3]> {
+    let mut coords2d: Vec<geo::Coordinate<f64>> = points
+        .iter()
+        .map(|c| geo::Coordinate { x: c[0], y: c[1] })
+        .collect::<Vec<geo::Coordinate<f64>>>();
+    let extreme_pts: geo::LineString<f64> =
+        geo::algorithm::convex_hull::quick_hull(coords2d.as_mut_slice());
+    fn same_point(c3: &[f64; 3], c2: &geo::Point<f64>) -> bool {
+        (c3[0] - c2.x()).abs() + (c3[1] - c2.y()).abs() < 0.00000001
+    }
+    fn is_extreme(c3: &[f64; 3], extreme_pts: &geo::LineString<f64>) -> bool {
+        extreme_pts
+            .clone()
+            .into_points()
+            .into_iter()
+            .any(|p| same_point(c3, &p))
+    }
+    points
+        .into_iter()
+        .filter(|p| is_extreme(p, &extreme_pts))
+        .collect::<Vec<[f64; 3]>>()
+}
+
+fn remove_spikes(trj: Vec<[f64; 3]>, spikes: Vec<[f64; 3]>) -> Vec<[f64; 3]> {
+    fn same_point(p: &[f64; 3], q: &[f64; 3]) -> bool {
+        // Equality of floats is intentional: they must be copies!
+        (p[0] == q[0]) && (p[1] == q[1])
+    }
+    let mut spikeless: Vec<[f64; 3]> = Vec::new();
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+    while i < spikes.len() {
+        let p = spikes[i];
+        let q = trj[j];
+        if same_point(&p, &q) {
+            i += 1;
+            j += 1;
+        } else {
+            j += 1;
+            spikeless.push(q);
+        }
+    }
+    while j < trj.len() {
+        spikeless.push(trj[j]);
+        j += 1;
+    }
+    spikeless
+}
+
+fn get_spikes(trj: Vec<[f64; 3]>) -> Vec<[f64; 3]> {
+    let mut spikes: Vec<[f64; 3]> = Vec::new();
+    fn is_spike(p: &[f64; 3], q: &[f64; 3], r: &[f64; 3]) -> bool {
+        (get_distance(p, q) > get_distance(p, r)) | (get_distance(q, r) > get_distance(p, r))
+    }
+    for i in 1..trj.len() - 1 {
+        if is_spike(&trj[i - 1], &trj[i], &trj[i + 1]) {
+            spikes.push(trj[i])
+        }
+    }
+    spikes
+}
+
+fn get_distance(from: &[f64; 3], to: &[f64; 3]) -> f64 {
+    // Returns haversine distance in meters
+    let start = geo::point!(x: from[0],y: from[1]);
+    let end = geo::point!(x:to[0], y:to[1]);
+    start.haversine_distance(&end)
+}
+
+#[cfg(test)]
+mod trajectory_builder_test {
+    use super::*;
+    #[test]
+    fn distance_test() {
+        // According to google these two points are approximately 2 km apart
+        let from = &[10.128126551731393, 55.39057912238903, 0.];
+        let to = &[10.159840991123847, 55.386813002794774, 1.];
+        let google_distance = 2000.;
+        assert!((get_distance(from, to) - google_distance).abs() < 50.);
+    }
+
+    #[test]
+    fn test_convexhull_track() {
+        // Colinear points are also removed, as they should be!
+        let trj = vec![
+            [0., 0., 0.],
+            [1.1, 0., 1.],
+            [2., 0., 2.],
+            [2., 1.1, 3.],
+            [2., 2., 4.],
+            [1., 2., 5.],
+            [0., 2., 6.],
+            [0., 1., 7.],
+            [0., 0., 8.],
+        ];
+        let hulltrack = vec![
+            [0., 0., 0.],
+            [2., 0., 2.],
+            [2., 2., 4.],
+            [0., 2., 6.],
+            [0., 0., 8.],
+        ];
+        let res = get_convex_hull_trj(trj);
+        assert_eq!(res, hulltrack);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use ch_filter::CHFilter;
 use chrono::NaiveDate;
 use regex::Regex;
 use std::io::{BufReader, Read};
-use trajectorybuilder::TrajectoryBuilder;
+mod ch_filter;
 mod tradis;
 mod trajectory;
-mod trajectorybuilder;
 use std::env;
 
 const MIN_VELOCITY: f64 = 2.5; // If the average velocity of WINDOWS_SIZE points is less than MIN_VELOCITY km/h the trj is cut.
@@ -22,25 +22,22 @@ fn main() -> std::io::Result<()> {
     let mut buf_reader = BufReader::new(std::io::stdin());
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
+    println!("file length {}", contents.len());
     // More parsers are probably needed, for now we only accept gpx
     let trj_stream: Vec<[f64; 3]> = parse_gpx(contents);
-    let mut builder = TrajectoryBuilder::new(window_size, MIN_VELOCITY);
+    let mut filtered = CHFilter::new(window_size, trj_stream.into_iter());
     let mut count = 0;
-    for coord in trj_stream {
-        if let Some(trj) = builder.handle_next(coord) {
-            if trj.len() > 1 {
-                count += 1;
-                trj.to_csv(format!("{}/trj{}_win{}.csv", args[1], count, window_size));
-                println!("wrote trj with len: {}", trj.to_array().len());
-            }
-        };
+    while let Some(pt) = filtered.next() {
+        count += 1;
+        print!("0 {:?}", pt);
     }
+    println!("\ncount:{}", count);
     Ok(())
 }
 
 fn parse_gpx(gpx: String) -> Vec<[f64; 3]> {
     let mut trj: Vec<[f64; 3]> = Vec::new();
-    let re_lat = Regex::new(r"lat=\W(\d+[[:punct:]]\d+)\W\slon=\W(\d+[[:punct:]]\d+)\W{2}<ele>\d+[[:punct:]]\d+</ele><time>(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})[[:punct:]](\d+)").unwrap();
+    let re_lat = Regex::new(r"lat=\W(\d+[[:punct:]]\d+)\W\slon=\W(\d+[[:punct:]]\d+)\W{2}[[:space:]]*<ele>\d+[[:punct:]]\d+</ele>[[:space:]]*<time>(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})[[:punct:]](\d+)").unwrap();
     for cap in re_lat.captures_iter(&gpx) {
         let lat: f64 = cap[1].parse::<f64>().unwrap();
         let lon: f64 = cap[2].parse::<f64>().unwrap();
@@ -56,5 +53,6 @@ fn parse_gpx(gpx: String) -> Vec<[f64; 3]> {
             .timestamp_millis() as f64;
         trj.push([lon, lat, time]); // Note we have x=lon, y=lat, z=time(ms)
     }
+    println!("parsed {} coords", trj.len());
     trj
 }
