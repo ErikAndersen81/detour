b90e39790e079140ed485f1ad510cbdc697b95b8
Erik Andersen <3rik4ndersen@gmail.com>
2021-10-22 17:01:53 +0200

    window cleaning

--- src/utility/motion_detector.rs
+++ src/utility/motion_detector.rs
@@ -36,9 +36,10 @@ impl MotionDetector {
             self.tmp_ivls.push(span);
             let span: f64 = self.tmp_ivls.clone().into_iter().sum();
             if span >= self.timespan {
-                // Throw away excess intervals
+                // The window is full
                 let mut excess: f64 = self.tmp_ivls[0];
-                while (span - excess) >= self.timespan {
+                // Throw away excess intervals
+                while (span - excess) > self.timespan {
                     self.spt_ivls.remove(0);
                     self.tmp_ivls.remove(0);
                     excess += self.tmp_ivls[0];
@@ -89,5 +90,38 @@ mod trajectory_builder_test {
         let to = &[10.159840991123847, 55.386813002794774, 1.];
         let google_distance = 2000.;
         assert!((get_distance(from, to) - google_distance).abs() < 50.);
+    }
+
+    #[test]
+    fn unfilled_window_test() {
+        // According to google these two points are approximately 2 km apart
+        let from = [10.128126551731393, 55.39057912238903, 0.];
+        let to = [10.159840991123847, 55.386813002794774, 1.];
+        let mut md = MotionDetector::new(3.0, 2.5, 0.5);
+        let mut is_moving = md.is_moving(from);
+        assert_eq!(None, is_moving);
+        is_moving = md.is_moving(to);
+        assert_eq!(None, is_moving);
+    }
+
+    #[test]
+    fn is_moving_test() {
+        // According to google these two points are approximately 2 km apart
+        // If travelling from a -> b takes 15 min = 900 000 ms
+        // then the travel speed is 8 km/h which should be higher than 2.5 km/h
+        // i.e. is_moving is true
+        let speed: f64 = 900000.0;
+        let a = [10.128126551731393, 55.39057912238903, 0.];
+        let b = [10.159840991123847, 55.386813002794774, speed];
+        let mut md = MotionDetector::new(speed, 2.5, 0.5);
+        let mut is_moving = md.is_moving(a);
+        assert_eq!(is_moving, None);
+        is_moving = md.is_moving(b);
+        assert_eq!(is_moving, Some(true));
+        // Staying at point b for slightly more than 15 minutes
+        // s.t. the first value is tossed
+        let c = [b[0], b[1], speed * 2.1];
+        is_moving = md.is_moving(c);
+        assert_eq!(is_moving, Some(false));
     }
 }
