b8601df0f2088695d2a0bbae76afd9810ce7235a
Erik Andersen <3rik4ndersen@gmail.com>
2021-10-23 11:03:01 +0200

    allocate on heap

--- src/datastructures/graph/edge.rs
+++ src/datastructures/graph/edge.rs
@@ -2,6 +2,13 @@ use super::{Trajectory, Vertex};
 
 #[derive(Clone)]
 pub(crate) struct Edge {
-    pub to: Vertex,
+    pub to: Box<Vertex>,
     pub trj: Trajectory,
+}
+
+impl Edge {
+    pub fn to_csv(&self, filename: String) -> std::io::Result<()> {
+        self.trj.to_csv(filename)?;
+        Ok(())
+    }
 }
