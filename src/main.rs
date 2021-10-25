d81d415dcc0b769f21799481ef4b407db6c81665
Erik Andersen <3rik4ndersen@gmail.com>
2021-10-25 22:31:46 +0200

    timeguard

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub(crate) use datastructures::Graph;
 use std::io::{BufReader, Read};
-use utility::CHFilter;
+use utility::{time_guard, CHFilter};
 mod datastructures;
 mod parser;
 mod utility;
@@ -10,7 +10,8 @@ use std::path::Path;
 fn main() {
     let config = std::fs::read_to_string("config.cfg");
     let config = parser::parse_config(config.unwrap());
-    println!("{}", config);
+    // uncomment to print current config
+    // println!("{}", config);
     let out_path = std::env::args()
         .nth(1)
         .unwrap_or_else(|| "output".to_owned());
@@ -25,9 +26,16 @@ fn main() {
         .expect("can't read from stdin");
     // More parsers are probably needed, for now we only accept gpx
     let stream: Vec<[f64; 3]> = parser::parse_gpx(contents);
-    let stream = CHFilter::new(config.window_size, stream.into_iter()).collect::<Vec<[f64; 3]>>();
+    let stream: Vec<[f64; 3]> = time_guard::clean_stream(stream);
+    let stream: Vec<[f64; 3]> =
+        CHFilter::new(config.window_size, stream.into_iter()).collect::<Vec<[f64; 3]>>();
     let graph = Graph::new(stream, config);
-    graph
-        .to_csv(String::from("vertices.csv"))
-        .expect("Could not write to csv!");
+    graph.into_iter().enumerate().for_each(|(i, path)| {
+        let subdir = format!("{}", i);
+        std::fs::create_dir(&subdir).unwrap();
+        env::set_current_dir(&subdir).unwrap();
+        path.to_csv(String::from("vertices.csv"))
+            .expect("Could not write to csv!");
+        env::set_current_dir("..").unwrap();
+    });
 }
