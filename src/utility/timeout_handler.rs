34e610c7ad7dd36d2ef4556c946e1f863388fd72
Erik Andersen <3rik4ndersen@gmail.com>
2021-11-24 11:33:25 +0100

    allowing dead code

--- src/utility/timeout_handler.rs
+++ src/utility/timeout_handler.rs
@@ -1,16 +1,18 @@
+#[allow(dead_code)]
 pub struct TimeoutHandler {
     connection_timeout: f64,
     last_read: [f64; 3],
 }
 
 impl TimeoutHandler {
+    #[allow(dead_code)]
     pub fn new(connection_timeout: f64, initial_point: &[f64; 3]) -> TimeoutHandler {
         TimeoutHandler {
             connection_timeout,
             last_read: *initial_point,
         }
     }
-
+    #[allow(dead_code)]
     pub fn is_alive(&mut self, point: &[f64; 3]) -> bool {
         let delta_time = point[2] - self.last_read[2];
         self.last_read = *point;
