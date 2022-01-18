2d49081c2edb99c1d08b6a8bd50aa4ade8bc9535
Erik Andersen <3rik4ndersen@gmail.com>
2022-01-18 08:26:29 +0100

    rm test mod

--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -32,16 +32,3 @@ pub use visvalingam::visvalingam;
 pub fn get_distance(from: &[f64; 3], to: &[f64; 3]) -> f64 {
     ((from[0] - to[0]).powi(2) + (from[1] - to[1]).powi(2)).sqrt()
 }
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    #[test]
-    fn distance_test() {
-        // According to google these two points are approximately 2 km apart
-        let from = &[10.128126551731393, 55.39057912238903, 0.];
-        let to = &[10.159840991123847, 55.386813002794774, 1.];
-        let google_distance = 2000.;
-        assert!((get_distance(from, to) - google_distance).abs() < 50.);
-    }
-}
