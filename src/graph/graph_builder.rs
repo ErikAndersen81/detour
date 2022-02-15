de8083cd13da496573c37df7f7d36014ce5cdd0f
Erik Andersen <3rik4ndersen@gmail.com>
2022-02-15 13:38:25 +0100

    Update description + less printlns

--- src/graph/graph_builder.rs
+++ src/graph/graph_builder.rs
@@ -9,12 +9,8 @@ use petgraph::stable_graph::StableDiGraph;
 use petgraph::EdgeDirection;
 use std::collections::HashSet;
 
-/// Returns a detour graph when given a vector of preprocessed streams.
-///
-/// First, streams are converted into paths using [get_paths](get_paths).
-/// Paths that does not contain at least one trajectory are removed.
-/// Then, each path is inserted into a detour graph and the nodes are
-/// merged. The graph is then made acyclic and finally the edges are merged.
+/// Constructs the graph
+/// Handles spatial node clustering, edge clustering and detecting less frequently visited places.
 pub fn get_graph(streams: Vec<Vec<[f64; 3]>>) -> DetourGraph {
     let mut graph = DetourGraph::new();
     let mut path_stats = PathBuilderStats::default();
@@ -23,11 +19,7 @@ pub fn get_graph(streams: Vec<Vec<[f64; 3]>>) -> DetourGraph {
         .flat_map(|stream| get_paths(stream, &mut path_stats))
         .filter(|path| path.len() > 1)
         .for_each(|path| graph.add_path(path));
-    println!("\ttest new merge");
     merge_nodes(&mut graph);
-    //println!("\tmerging nodes");
-    //graph.merge_nodes();
-    println!("\tmerging edges");
     merge_edges(graph.get_mut_graph());
     println!("Path builder stats:\n{}", path_stats);
     graph
