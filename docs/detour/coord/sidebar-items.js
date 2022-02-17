d574ac780b96f8f4afd02a5354d2f4953ae0eebb
Erik Andersen <3rik4ndersen@gmail.com>
2022-02-17 11:54:00 +0100

    update docs

--- docs/detour/coord/sidebar-items.js
+++ docs/detour/coord/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["DEG",""],["RAD",""],["RADIUS",""]],"fn":[["from_epsg_3857_to_4326",""],["from_epsg_4326_to_3857",""]]});
+initSidebarItems({"constant":[["DEG",""],["RAD",""],["RADIUS","Constants for WSG 84 / Pseudo-Mercator (EPSG CRS Code 3857)"]],"fn":[["from_epsg_3857_to_4326","Convert a coordinate of EPSG 3857 `[easting, northing, time]` to  `[lat, lon, time]` in EPSG 4326"],["from_epsg_4326_to_3857","Convert a coordinate of `[lat, lon, time]` in EPSG 4326 to EPSG 3857 `[easting, northing, time]`"]]});
